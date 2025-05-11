6e788d004d7945f6e25f55bc05530fdcb04a02f8
Givi Jain <131352767+givijain1234@users.noreply.github.com>
2025-05-11 16:24:37 +0530

    Create WebhookResponse.java

---
+++ src/main/java/com.example.webhookdemo/model/WebhookResponse.java
@@ -0,0 +1,22 @@
+package com.example.webhookdemo.model;
+
+public class WebhookResponse {
+    private String webhook;
+    private String accessToken;
+
+    public String getWebhook() {
+        return webhook;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setWebhook(String webhook){
+        this.webhook = webhook;
+    }  
+
+    public void setAccessToken(String accessToken){
+        this.accessToken = accessToken;
+    }  
+}
